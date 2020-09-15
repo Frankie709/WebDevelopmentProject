6ed3fbdad33ff76e9b4cf3a468b47539f76cf3ba
Frankie709 <46513075+Frankie709@users.noreply.github.com>
2020-09-15 17:30:17 -0600

    Add files via upload

---
+++ js/list.js
@@ -0,0 +1,6 @@
+var list = document.getElementById("aList");
+list.addEventListener("click", myFunction);
+
+function myFunction(){
+    alert("Now you're ready to head to the bookstore! Enjoy!");
+}
---
+++ js/submit.js
@@ -0,0 +1,6 @@
+var list = document.getElementById("iSubmit");
+list.addEventListener("click", myFunction);
+
+function myFunction(){
+    alert("Thank you for your recommendation! Be sure to check out Ahsoka's list!");
+}
